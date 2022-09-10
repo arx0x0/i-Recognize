4257d9e83fc85c5f4db29158fc7e5025320f7f2c
arx0x0 <rahimshami11@icloud.com>
2022-09-10 17:36:17 -0400

    OK finally done

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -47,6 +47,11 @@ class Signin extends React.Component {
   render() {
     const { onRouteChange } = this.props;
     return ( //Sign in form from tachyons templates
+      <div>
+        <div> 
+           <h1>i-Recognize</h1>
+            <h3>Face Recognition App</h3>
+            </div>
       <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
         <main className="pa4 black-80">
           <div className="measure">
@@ -87,6 +92,7 @@ class Signin extends React.Component {
           </div>
         </main>
       </article>
+      </div>
     );
   }
 }
